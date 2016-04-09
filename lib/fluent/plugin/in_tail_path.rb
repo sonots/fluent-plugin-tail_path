7caebc8b3eb8f7813887e5cacf84a8fcae17f9ff
sonots <sonots@gmail.com>
2016-04-10 00:23:33 +0900

    modify namespace

--- lib/fluent/plugin/in_tail_path.rb
+++ lib/fluent/plugin/in_tail_path.rb
@@ -1,32 +1,32 @@
 require 'fluent/plugin/in_tail'
 
-class RouterWrap
-  attr_accessor :tail_watcher
-
-  def initialize(router, path_key)
-    @router = router
-    @path_key = path_key
-    @tail_watcher = nil
-  end
-
-  def emit(tag, time, record)
-    record[@path_key] = @tail_watcher.path unless @path_key.nil?
-    @router.emit(tag, time, record)
-  end
-
-  def emit_array(tag, array)
-    @router.emit_array(tag, array)
-  end
-
-  def emit_stream(tag, es)
-    es.each {|time,record|
-      record[@path_key] = @tail_watcher.path unless @path_key.nil?
-    }
-    @router.emit_stream(tag, es)
-  end
-end
-
 class Fluent::NewTailPathInput < Fluent::NewTailInput
+  class RouterWrap
+    attr_accessor :tail_watcher
+
+    def initialize(router, path_key)
+      @router = router
+      @path_key = path_key
+      @tail_watcher = nil
+    end
+
+    def emit(tag, time, record)
+      record[@path_key] = @tail_watcher.path unless @path_key.nil?
+      @router.emit(tag, time, record)
+    end
+
+    def emit_array(tag, array)
+      @router.emit_array(tag, array)
+    end
+
+    def emit_stream(tag, es)
+      es.each {|time,record|
+        record[@path_key] = @tail_watcher.path unless @path_key.nil?
+      }
+      @router.emit_stream(tag, es)
+    end
+  end
+
   Fluent::Plugin.register_input('tail_path', self)
 
   config_param :path_key, :string, :default => nil
@@ -41,4 +41,3 @@ class Fluent::NewTailPathInput < Fluent::NewTailInput
     super
   end
 end
-
